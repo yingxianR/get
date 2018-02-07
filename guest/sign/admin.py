44ee6a7e39e074fe8603a5bd420686138b5ae24d
sunjian01 <sunjian01@sv.remaintech.com>
2018-02-07 18:00:28 +0800

    2018.2.7

--- guest/sign/admin.py
+++ guest/sign/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from sign.models import Event, Guest
 # Register your models here.
+
+class EventAdmin(admin.ModelAdmin):
+	list_display = ['name','status','start_time','id']
+
+class GuestAdmin(admin.ModelAdmin):
+	list_display = ['realname','phone','email']
+
+admin.site.register(Event)
+admin.site.register(Guest)
+
--- guest/sign/views.py
+++ guest/sign/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
 	return render(request,"index.html")
@@ -10,14 +12,18 @@ def login_action(request):
 	if request.method == 'POST':
 		username = request.POST.get('username','')
 		password = request.POST.get('password','')
-		if username == 'admin' and password == 'admin123':
-			response = HttpResponseRedirect('/event_manage/')
+		user = auth.authenticate(username=username,password=password)
+		#if username == 'admin' and password == 'admin123':
+		if user is not None:
+			auth.login(request,user) #登录
 			#response.set_cookie('user',username,3600) #设置cookie;
 			request.session['user'] = username #设置session;
+			response = HttpResponseRedirect('/event_manage/')
 			return response
 		else: 
 			return render(request,'index.html',{'error':'username or password error!'})
 #发布会管理
+@login_required
 def event_manage(request):
 	#username = request.COOKIES.get('user','') #读取 浏览器 cookie
 	username = request.session.get('user','') #读取 浏览器 session
