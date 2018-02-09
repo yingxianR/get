b802a3af83cd0b8947424c2efce84ce59a547cca
sunjian01 <sunjian01@sv.remaintech.com>
2018-02-09 16:59:24 +0800

    18.2.9

--- guest/sign/admin.py
+++ guest/sign/admin.py
@@ -4,10 +4,13 @@ from sign.models import Event, Guest
 
 class EventAdmin(admin.ModelAdmin):
 	list_display = ['name','status','start_time','id']
+	search_fields = ['name'] #搜索栏
+	list_filter = ['status'] #过滤器
 
 class GuestAdmin(admin.ModelAdmin):
-	list_display = ['realname','phone','email']
-
-admin.site.register(Event)
-admin.site.register(Guest)
+	list_display = ['realname','phone','email','sign','create_time','event']
+	search_fields = ['realname','phone'] #搜索栏
+	list_filter = ['sign'] #过滤器
+admin.site.register(Event,EventAdmin)
+admin.site.register(Guest,GuestAdmin)
 
